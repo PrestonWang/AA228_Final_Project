17727d0882f7fdc081a62de186327e1c13662e78
PrestonWang <prestonwang95@gmail.com>
2019-11-12 22:16:45 -0800

    Updated comments to the plane class

--- plane.m
+++ plane.m
@@ -1,37 +1,38 @@
+% Colin Shi, Preston Wang, and Nathan Wei
+% AA 228 Final Project, Fall 2019
+% Plane class: represents plane in 2D grid-world and allows it to move in
+% heading based on waypoint. 
+
 classdef plane < handle
     %2D kinematic plane model
-    %   8 states: (x,y,theta,m xdot, ydot, thetadot, mdot), where m is
-    %   equal to fuel remaining
-    %   2 controls: u(yaw rate) and v(velocity
-    %   Properties: v_max, v_cruise, v_stall, max_yaw, fuel_rate, initcon,
-    %   gains
+    %   4 states: (x,y,theta,m) where m is equal to fuel remaining
+    %   Properties: fuel_rate, initcon,
     
     properties
         v = 1; % miles/min
-        initcon;
+        initcon; % initial conditions [x0; y0; theta0; m0]
         fuel_rate = 5000/60 % lbs/min
-        state;
-        state_past;
+        state; % [x;y;theta;m]
+        state_past; % matrix of previous states
     end
     
     methods
         function obj = plane(initcon)
-            %UNTITLED2 Construct an instance of this class
-            %   Detailed explanation goes here
+            %Construct a plane object with initial conditions
             obj.initcon = initcon;
             obj.state = initcon;
             obj.state_past = initcon;
         end
         
         function theta = calcHeading(obj,target)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
+            %calcHeading calculates the new theta based on the target
+            %waypoint location
             theta = atan2(target(2)-obj.state(2), target(1)-obj.state(1));
         end
         function [new_state] = calcState(obj,t,target)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            theta = atan2(target(2)-obj.state(2), target(1)-obj.state(1));
+            %calcState returns the new states based on the new target
+            %location and a timestep of t. 
+            theta = obj.calcHeading(target);
             new_state = [obj.state(1)+cos(theta)*obj.v*t; obj.state(2)+sin(theta)*obj.v*t; theta; obj.state(4) - obj.fuel_rate*t];
             obj.state = new_state;
             obj.state_past = [obj.state_past, new_state];
