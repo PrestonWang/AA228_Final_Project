d4b69b328f90557cf97d713ee95bb370fb3bf5b9
wrongwei <njwei23@gmail.com>
2019-12-04 13:50:44 -0800

    Added bootstrapping and more stats to process_data.m

--- process_data.m
+++ process_data.m
@@ -1,14 +1,14 @@
-% load experiments/Experiment1_20191203_MDP_N10000_dt5mins % MDP dataset for storm with uniform transition
+load experiments/Experiment1_20191203_MDP_N10000_dt5mins % MDP dataset for storm with uniform transition
 % load experiments/Experiment1_20191203_AStar_N10000_dt5mins % AStar dataset for storm with uniform transition
 % load experiments/Experiment2_20191203_MDP_N10000_dt5mins % MDP dataset for storm headed in random direction
 % load experiments/Experiment2_20191203_AStar_N10000_dt5mins % AStar dataset for storm headed in random direction
 % load experiments/Experiment3_20191203_MDP_N10000_dt5mins % MDP dataset for double storm cost
 % load experiments/Experiment3_20191203_AStar_N10000_dt5mins % AStar dataset for double storm cost
-% titleString = 'MDP Delay Statistics with Uniform Storm Transition Probabilities';
+titleString = 'MDP Delay Statistics with Uniform Storm Transition Probabilities';
 % titleString = 'A* Delay Statistics with Uniform Storm Transition Probabilities';
 % titleString = 'MDP Delay Statistics with a Storm Moving in a Single Random Direction';
 % titleString = 'A* Delay Statistics with a Storm Moving in a Single Random Direction';
-titleString = 'MDP Delay Statistics with Doubling Storm Cost';
+% titleString = 'MDP Delay Statistics with Doubling Storm Cost';
 % titleString = 'A* Delay Statistics with Doubling Storm Cost';
 numExp = size(percentDelay);
 stats = {};
@@ -31,6 +31,16 @@ legend(line([avg, avg], [0, 1],'Color','red','LineStyle','--','LineWidth',2),...
 title(titleString);
 xlabel('Percent Increase in Flight Time')
 ylabel('Fraction of Delayed Flights')
-xlim([5, 70]);
+xlim([5, 80]);
 ylim([0, 0.5]);
-fprintf('N_D / N = %.2f%%\n', 100*(count-1)/numExp(1));
+
+N_D = count-1;
+fprintf('N_D / N = %.2f%%\n', 100*N_D/numExp(1));
+sampleStdv = mean(bootstrp(N_D*10, @std, significantDelay));
+fprintf('Average delay (over N_D) after %d iterations: %.2f +/- %.2f%%\n', ...
+    N_D, avg, sampleStdv);
+% fprintf('Standard deviation as percent of mean: %.2f%%\n', ...
+%     100*sampleStdv/avg);
+sampleStdv2 = mean(bootstrp(numExp(1)*10, @std, percentDelay*100));
+fprintf('Average delay (over N) after %d iterations: %.2f +/- %.2f%%\n', ...
+    numExp(1), mean(percentDelay)*100, sampleStdv2);
